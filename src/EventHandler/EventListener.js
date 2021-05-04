7c83281d121b5276f18024224489f92312957275
stefftek <spfahler@t-online.de>
2021-05-04 16:39:14 +0200

    Anpassung weil Clärchen

--- src/EventHandler/EventListener.js
+++ src/EventHandler/EventListener.js
@@ -13,21 +13,6 @@ module.exports = (client) => {
     /* Startup Event */
     client.on('ready', () => {
         ready(client);
-
-        client.ws.on('INTERACTION_CREATE', async interaction => {
-            const commandName = interaction.data.name.toLowerCase();
-            const args = interaction.data.options;
-            const author = interaction.member.user
-            const channel = await client.channels.fetch(interaction.channel_id)
-    
-            const command = client.commands.get(commandName)
-            try {
-                command.execute({"client": client, "author": author, "channel": channel, "args": args, "interaction": interaction});
-            } catch (error) {
-                console.error(error);
-                channel.send("An error occured while trying to execute that command.");
-            }
-        });
     });
 
     /*
--- src/EventHandler/Events/Ready.js
+++ src/EventHandler/Events/Ready.js
@@ -2,4 +2,19 @@ const logger = require("../../Utils/Logger");
 
 module.exports = (client) => {
     logger.done(`Logged in as ${client.user.tag}!`);
+
+    client.ws.on('INTERACTION_CREATE', async interaction => {
+        const commandName = interaction.data.name.toLowerCase();
+        const args = interaction.data.options;
+        const author = interaction.member.user
+        const channel = await client.channels.fetch(interaction.channel_id)
+
+        const command = client.commands.get(commandName)
+        try {
+            command.execute({"client": client, "author": author, "channel": channel, "args": args, "interaction": interaction});
+        } catch (error) {
+            console.error(error);
+            channel.send("An error occured while trying to execute that command.");
+        }
+    });
 }
